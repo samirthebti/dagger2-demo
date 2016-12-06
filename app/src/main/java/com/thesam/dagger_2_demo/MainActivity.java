83e81d141d2c527105f3b6b2529374c13826432b
samirthebti <thebtisam@gmail.com>
2016-12-07 00:32:29 +0100

    feat : inject the component

--- app/src/main/java/com/thesam/dagger_2_demo/MainActivity.java
+++ app/src/main/java/com/thesam/dagger_2_demo/MainActivity.java
@@ -1,20 +1,20 @@
 package com.thesam.dagger_2_demo;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import javax.inject.Inject;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    @Inject
-    SharedPreferences sharedPreferences;
-
+    Vehicle vehicle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        InjectorClass.inject(this);
         setContentView(R.layout.activity_main);
+        VehiculInterface component = DaggerVehiculInterface.builder().vehiculeModel(new VehiculeModel()).build();
+
+        vehicle = component.provideVehicule();
+
+        Toast.makeText(this, String.valueOf(vehicle.getSpeed()), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/thesam/dagger_2_demo/VehiculeModel.java
+++ app/src/main/java/com/thesam/dagger_2_demo/VehiculeModel.java
@@ -2,13 +2,14 @@ package com.thesam.dagger_2_demo;
 
 import javax.inject.Singleton;
 
+import dagger.Module;
 import dagger.Provides;
 
 /**
  * Created by Samir Thebti  on 6/12/16.
  * ----->> thebtisam@gmail.com <<-----
  */
-
+@Module
 public class VehiculeModel {
     @Provides
     @Singleton
